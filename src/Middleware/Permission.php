a8c66af5e62689830cb874f624eccd90e4b9bcd8
helingfeng <857190327@qq.com>
2019-04-04 16:26:57 +0800

    Fix: 修复中间件问题

--- src/Middleware/Permission.php
+++ src/Middleware/Permission.php
@@ -10,11 +10,11 @@ class Permission
     public function handle($request, Closure $next, $permission)
     {
         $permissions = explode('|', $permission);
-        $config_permissions = Auth::guard(config('permission.guard'))->user()->allPermissions()->toArray();
+        $config_permissions = Auth::guard(config('permission.guard'))->user()->allPermission()->toArray();
         if (!collect($permissions)->first(function ($value) use ($config_permissions) {
             return in_array($value, $config_permissions);
         })) {
-            return 'access deny.';
+            return response('access deny.');
         }
 
         return $next($request);
